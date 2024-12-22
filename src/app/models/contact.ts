59d4f360dea720e968014edf7695e0c9340b1ec9
ezqel <ezekiel3499@gmail.com>
2024-12-22 23:36:40 +0300

    Install rxdb and create db service

---
+++ src/app/models/contact.ts
@@ -0,0 +1,51 @@
+import {
+  toTypedRxJsonSchema,
+  ExtractDocumentTypeFromTypedRxJsonSchema,
+  RxJsonSchema,
+} from 'rxdb';
+
+export const contactSchemaLiteral = {
+  version: 0,
+  primaryKey: 'id',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      maxLength: 100,
+    },
+    firstName: {
+      type: 'string',
+    },
+    lastName: {
+      type: 'string',
+    },
+    phoneNumber: {
+      type: 'string',
+    },
+    email: {
+      type: 'string',
+    },
+    physicalAddress: {
+      type: 'string',
+    },
+    photo: {
+      type: 'string',
+    },
+    isFavourited: {
+      type: 'boolean',
+    },
+  },
+  // required: ['phoneNumber'],
+  // indexes: ['phoneNumber'],
+} as const;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const schemaTyped = toTypedRxJsonSchema(contactSchemaLiteral);
+
+// aggregate the document type from the schema
+export type Contact = ExtractDocumentTypeFromTypedRxJsonSchema<
+  typeof schemaTyped
+>;
+
+// create the typed RxJsonSchema from the literal typed object.
+export const contactSchema: RxJsonSchema<Contact> = contactSchemaLiteral;
---
+++ src/app/services/rxdb.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { createRxDatabase, RxCollection, RxDatabase, RxDocument } from 'rxdb';
+import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
+import { from, shareReplay } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { Contact, contactSchemaLiteral } from '../models/contact';
+
+export type ContactDocument = RxDocument<Contact>;
+export type ContactCollection = RxCollection<Contact>;
+export interface DatabaseCollections {
+  contacts: ContactCollection;
+}
+export type ContactsDatabase = RxDatabase<DatabaseCollections>;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RxdbService {
+  contactsDb$ = from(this.initDb()).pipe(shareReplay(1));
+
+  async initDb() {
+    const db = await createRxDatabase<DatabaseCollections>({
+      name: 'contacts',
+      storage: getRxStorageDexie(),
+    });
+
+    const collections = await db.addCollections({
+      contacts: {
+        schema: contactSchemaLiteral,
+      },
+    });
+
+    collections.contacts.preInsert((doc) => {
+      if (!doc.id) {
+        doc.id = uuidv4();
+      }
+    }, false);
+
+    return collections.contacts;
+  }
+}
