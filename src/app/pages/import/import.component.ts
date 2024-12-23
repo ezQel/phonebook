9f233a0d2ada8eac407d2540933bf9dee639afc3
ezqel <ezekiel3499@gmail.com>
2024-12-23 09:55:27 +0300

    importing

---
+++ src/app/pages/import/import.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-import',
+  imports: [MatIcon],
+  templateUrl: './import.component.html',
+})
+export class ImportComponent {}
